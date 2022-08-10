1768aa5a3bc3d4bd9e35e90c0fac3dad4fd78111
PearAmp <pearamp1999@gmail.com>
2022-08-10 18:02:15 +0700

    addrrr

--- lib/page/page3/components/button_popup.dart
+++ lib/page/page3/components/button_popup.dart
@@ -66,7 +66,7 @@ class _ButtonPopupState extends State<ButtonPopup> {
                           style: ElevatedButton.styleFrom(
                               minimumSize: const Size(20, 20),
                               shadowColor: Colors.transparent,
-                              primary: Colors.grey),
+                              primary: Styles.maincolor),
                           child: Padding(
                             padding: const EdgeInsets.only(bottom: 5),
                             child: Text(
@@ -95,12 +95,127 @@ class _ButtonPopupState extends State<ButtonPopup> {
                             Row(
                               children: [
                                 Container(
+                                  width: 70,
                                   decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.red,
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.black,
                                   ),
-                                  height: 100,
-                                  child: Text('kkk'),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text('Kbab',
+                                          style: Styles.textcontantEn
+                                              .copyWith(color: Colors.white))),
+                                ),
+                                Gap(10),
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.white,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text(
+                                    'Kbab',
+                                    style: Styles.textcontantEn,
+                                  )),
+                                ),
+                                Gap(10),
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.white,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text(
+                                    'Kbab',
+                                    style: Styles.textcontantEn,
+                                  )),
+                                ),
+                                Gap(10),
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.white,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text(
+                                    'Kbab',
+                                    style: Styles.textcontantEn,
+                                  )),
+                                ),
+                              ],
+                            ),
+                            const Gap(20),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.black,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text('Kbab',
+                                          style: Styles.textcontantEn
+                                              .copyWith(color: Colors.white))),
+                                ),
+                                const Gap(10),
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.white,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text(
+                                    'Kbab',
+                                    style: Styles.textcontantEn,
+                                  )),
+                                ),
+                                const Gap(10),
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.black,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text(
+                                    'Kbab',
+                                    style: Styles.textcontantEn
+                                        .copyWith(color: Colors.white),
+                                  )),
+                                ),
+                                const Gap(10),
+                                Container(
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.black,
+                                  ),
+                                  height: 30,
+                                  child: Center(
+                                      child: Text(
+                                    'Kbab',
+                                    style: Styles.textcontantEn
+                                        .copyWith(color: Colors.white),
+                                  )),
                                 ),
                               ],
                             ),
--- lib/page/page4/fourth_page.dart
+++ lib/page/page4/fourth_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:restaurant_app/theme/theme_style.dart';
+import 'package:flutter_facebook_app_links/flutter_facebook_app_links.dart';
 
 class FourthPage extends StatefulWidget {
   const FourthPage({Key? key}) : super(key: key);
@@ -11,7 +14,74 @@ class _FourthPageState extends State<FourthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Styles.maincolor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            Text(
+              'Pear\nRestaurant',
+              style: Styles.textcontantEn.copyWith(fontSize: 20),
+            ),
+            const Gap(20),
+            Column(
+              children: [
+                Text(
+                  'Create an account or login',
+                  style: Styles.textcontantEn.copyWith(fontSize: 20),
+                ),
+                const Gap(10),
+                Text('Login below or create a new\n PearRestaurant account',
+                    style: Styles.textcontantEn),
+                const Gap(20),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Text(
+                      'Continue with Google',
+                      style: Styles.textcontantEn.copyWith(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const Gap(10),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    color: Colors.black,
+                    child: Text(
+                      'Continue with Apple',
+                      style: Styles.textcontantEn
+                          .copyWith(fontSize: 18, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const Gap(10),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    color: Colors.blue.shade700,
+                    child: Text(
+                      'Continue with Facebook',
+                      style: Styles.textcontantEn
+                          .copyWith(fontSize: 18, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
